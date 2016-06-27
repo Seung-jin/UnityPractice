636f6c8af6ac703e5a998f3d442dfd51fe9452e7
LeeSeungjin <fewfree12@naver.com>
2016-06-27 16:06:54 +0900

    PickUp 획득 및 점수 출력

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+
+static class StaticNumber
+{
+    public const int MaxPickUp = 12;
+}
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
+    private int count;
+
     public float speed;
+    public Text countText;
+    public Text winText;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
 	}
 	
 	// Update is called once per frame
@@ -24,7 +37,29 @@ public class PlayerController : MonoBehaviour {
 
         //움직이는 방향 결정
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
         rb.AddForce(movement * speed);
-        
+    }
+
+    //플레이어가 오브젝트와 충돌했을 때
+    void OnTriggerEnter(Collider other)
+    {
+        //CompareTag : 태그를 확인
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            //게임 오브젝트 활성화 여부
+            other.gameObject.SetActive(false);
+            count += 1;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count : " + count.ToString();
+        if (count >= StaticNumber.MaxPickUp)
+        {
+            winText.text = "You Win!!";
+        }
     }
 }
