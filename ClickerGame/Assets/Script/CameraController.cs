baf316000e619ad1e02ca13e49cdfd2997c62e65
LeeSeungjin <fewfree12@naver.com>
2016-06-27 17:33:14 +0900

    클리커 게임 제작 시작 버튼 클릭 시 카운트 증가

---
+++ ClickerGame/Assets/Script/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+    
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(0, 0, 500, 700), "", GUIStyle.none)) {
+            player.GetComponent<PlayerController>().isClicked = true;
+        }
+    }
+}
---
+++ ClickerGame/Assets/Script/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public bool isClicked;
+
+    public int count;
+    public Text clickCountText;
+
+	// Use this for initialization
+	void Start () {
+        count = 0;
+        setClickCountText();
+        isClicked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isClicked)
+        {
+            count++;
+            setClickCountText();
+            isClicked = false;
+        }
+	}
+
+    void setClickCountText()
+    {
+        clickCountText.text = "Click Count : " + count.ToString();
+    }
+}
