c1be39128561be2f4990aac28bc9741821282abf
LeeSeungjin <fewfree12@naver.com>
2016-06-27 12:38:01 +0900

    카메라가 공을 따라서 시점 이동

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        //카메라와 플레이어 사이의 거리
+        offset = transform.position - player.transform.position;
+	}
+	
+    //카메라 위치 업데이트는 Update()가 아닌 LateUpdate()로
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
