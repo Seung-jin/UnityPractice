d19ab821252a05cd40ca9eafaa713f6eaf773237
LeeSeungjin <fewfree12@naver.com>
2016-07-05 12:59:09 +0900

    뛰는 애니메이션, 점프 애니메이션 적용

--- ClickerGame/Assets/Scripts/PlayerController.cs
+++ ClickerGame/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,18 @@ using System.Collections;
 static class defineData
 {
     public const int MaximumJump = 2;   //연속 점프 가능 횟수
-    public const float BasicSpeed = 3.0f; //player 구체의 기본 스피드
+    public const float BasicSpeed = 2.0f; //player 구체의 기본 스피드
     public const float JumpForce = 8.0f;    //점프 시 위로 가해지는 힘
 }
 
 public class PlayerController : MonoBehaviour {
     private float totalSpeed;   //player 구체의 총 스피드
     private float addSpeed;     //player 구체에 더해지는 스피드
+    private bool checkRun;      //player가 뛰고 있는 상태인지 체크
     private Rigidbody rigidbody;
     private Vector3 movement;
     private GameObject ground;
+    private Animator playerAnimator;
 
     public bool isSpeedClicked;
     public bool isJumpedClicked;
@@ -28,10 +30,12 @@ public class PlayerController : MonoBehaviour {
 	void Start () {
         totalSpeed = 0.0f;
         addSpeed = 0.0f;
-        jumpCount = 0;
+        checkRun = true;
         rigidbody = GetComponent<Rigidbody>();
         ground = GameObject.FindGameObjectWithTag("Ground");
+        playerAnimator = GetComponent<Animator>();
 
+        jumpCount = 0;
         clickCount = 0;
         coinCount = 0;
         updateClickCountText();
@@ -54,6 +58,8 @@ public class PlayerController : MonoBehaviour {
 
         //코인 갯수 업데이트
         clickCoinText.text = "Coin : " + coinCount.ToString();
+
+        UpdateAnimator();
 	}
 
     //클릭 갯수 업데이트
@@ -82,6 +88,8 @@ public class PlayerController : MonoBehaviour {
     {
         if (!isJumpedClicked)
             return;
+
+        checkRun = false;
 
         //연속 점프는 2번까지만 가능
         if (jumpCount <= defineData.MaximumJump)
@@ -114,16 +122,25 @@ public class PlayerController : MonoBehaviour {
 
     void OnCollisionEnter(Collision other)
     {
-        //바닥에 부딪혔을 때
+        //바닥에 부딪혔을 때 점프 카운트 설정
         if (other.gameObject == GameObject.FindGameObjectWithTag("Ground"))
         {
+            checkRun = true;
             if (jumpCount < defineData.MaximumJump)
+            {
                 JumpCountReset();
+            }
             else if (jumpCount >= defineData.MaximumJump)
             {
                 jumpCount = 1;
                 Invoke("JumpCountReset", 2);
             }
         }
+    }
+
+    void UpdateAnimator()
+    {
+        print("checkRun : " + checkRun);
+        playerAnimator.SetBool("isRun", checkRun);
     }
 }
